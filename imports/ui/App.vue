1240c31f3da0d09b91fbdabbfa937be5e76bc144
Ong Bona <bonasmart19@gmail.com>
2021-07-05 10:05:00 +0700

    Layout

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <div>
-    <router-view />
-    <router-link to="/">Home</router-link>
-    <router-link to="/user">User</router-link>
-    <router-link to="/customer">Customer</router-link>
-    <router-link to="/item">Item</router-link>
-  </div>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="drawer = !drawer" />
+
+        <q-toolbar-title> Master IT Shop </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="drawer" show-if-above bordered content-class="bg-grey-3">
+      <q-scroll-area class="fit">
+        <q-list>
+          <q-item clickable v-ripple to="/" exact>
+            <q-item-section avatar>
+              <q-icon name="home" />
+            </q-item-section>
+            <q-item-section> Home </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple to="/user">
+            <q-item-section avatar>
+              <q-icon name="person" />
+            </q-item-section>
+            <q-item-section> User </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple to="/customer">
+            <q-item-section avatar>
+              <q-icon name="group" />
+            </q-item-section>
+            <q-item-section> Customer </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
 </script>
-
-<style>
-</style>
