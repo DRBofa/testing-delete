32b9d19e9a3303ea6b3bbbc3c5a285a5165aa27e
Ong Bona <bonasmart19@gmail.com>
2021-07-05 15:30:55 +0700

    User Form

---
+++ imports/ui/components/UserForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="row justify-around q-ma-sm">
+      <div class="col-md-5 col-sm-12 q-ma-md">
+        <q-input outlined v-model="form.username" label="Username" />
+      </div>
+      <div class="col-md-5 col-sm-12 q-ma-md">
+        <q-input outlined v-model="form.email" label="Email" />
+      </div>
+      <div class="col-md-5 col-sm-12 q-ma-md">
+        <q-input outlined v-model="form.phone" label="Phone" />
+      </div>
+      <div class="col-md-5 col-sm-12 q-ma-md">
+        <q-select
+          outlined
+          v-model="form.role"
+          label="Role"
+          :options="roles"
+          emit-value
+          map-options
+          option-label="label"
+        />
+      </div>
+      <div class="col-md-5 col-sm-12 q-ma-md">
+        <q-input
+          outlined
+          v-model="form.password"
+          label="Password"
+          type="password"
+        />
+      </div>
+      <div class="col-md-5 col-sm-12 q-ma-md">
+        <q-input
+          outlined
+          v-model="confirmPassword"
+          label="Confirm"
+          type="password"
+        />
+      </div>
+    </div>
+    <div class="text-center">
+      <q-btn label="Submit" color="primary" @click="handleSubmit" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: null,
+        email: null,
+        phone: null,
+        role: null,
+        password: null,
+      },
+      confirmPassword: null,
+      roles: [
+        {
+          label: "Admin",
+          value: "admin",
+        },
+        {
+          label: "Sale",
+          value: "sale",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log("form:", this.form);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- imports/ui/pages/User.vue
+++ imports/ui/pages/User.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>User Page</h1>
+    <user-form />
   </div>
 </template>
 
 <script>
-export default {};
+import UserForm from "../components/UserForm.vue";
+export default {
+  components: { UserForm },
+};
 </script>
 
 <style>
