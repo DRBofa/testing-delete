494bb5d5275bf84401278d679f172c141a8370f1
Ong Bona <bonasmart19@gmail.com>
2021-07-14 08:18:19 +0700

    collections+methods

--- imports/ui/pages/Test.vue
+++ imports/ui/pages/Test.vue
@@ -2,12 +2,42 @@
   <div>
     <h1>Test</h1>
     <q-btn label="Inser Customer" @click="handleInsert" />
+    <q-btn label="Find Customer" @click="handleFind" />
+    <q-btn label="Update Customer" @click="handleUpdate" />
+    <ul>
+      <li v-for="(customer, index) in customers" :key="index">
+        {{ customer._id }} | {{ customer.name }} | {{ customer.age }}
+        <q-btn
+          icon="delete"
+          dense
+          color="red"
+          @click="handleDelete(customer._id)"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      customers: [],
+    };
+  },
   methods: {
+    handleUpdate() {
+      let doc = {
+        _id: "ec5oZmxNg3LrfN8kG",
+        name: "Jing Jing",
+        age: 18,
+      };
+      Meteor.call("customer.update", doc, (err, result) => {
+        if (result) {
+          this.handleFind();
+        }
+      });
+    },
     handleInsert() {
       let doc = {
         name: "Biona",
@@ -18,6 +48,23 @@ export default {
           console.log("result:", result);
         } else {
           console.log("err", err);
+        }
+      });
+    },
+    handleFind() {
+      Meteor.call("customer.find", (err, result) => {
+        if (result) {
+          console.log("result:", result);
+          this.customers = result;
+        } else {
+          console.log("err", err);
+        }
+      });
+    },
+    handleDelete(id) {
+      Meteor.call("customer.remove", id, (err, result) => {
+        if (result) {
+          this.handleFind();
         }
       });
     },
